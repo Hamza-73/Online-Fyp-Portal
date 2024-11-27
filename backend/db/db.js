fe998ec74ab442deaf24cea43811e778987a5615
Ameer Hamza Khan <101973302+Hamza-73@users.noreply.github.com>
2024-11-28 00:56:35 +0500

    Update db.js

--- backend/db/db.js
+++ backend/db/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDatabase = () => {
     
 
-  mongoose.connect(process.env.MONGOURI).then(() => {
+  mongoose.connect("mongodb+srv://hamza:hamza@fyp-portal.sa1tw.mongodb.net/fyp-portal?retryWrites=true&w=majority&appName=fyp-portal").then(() => {
     console.log("Connected to database");
   }).catch((err) => {
     console.log("Database connection error:", err.message);
